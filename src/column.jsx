3cb7890a50188ccc0d801ddb78385a40b83970f7
pi3rrot <4dakto@gmail.com>
2018-12-09 13:40:26 +0900

    reorder a list.

--- src/column.jsx
+++ src/column.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import styled from 'styled-components';
-import Task from './task'; 
+import { Droppable } from 'react-beautiful-dnd';
+import Task from './task';
 
 const Container = styled.div`
   margin: 8px;
@@ -19,9 +20,19 @@ class Column extends Component {
     return (
       <Container>
         <Title>{this.props.column.title}</Title>
-        <TaskList>
-          {this.props.tasks.map(task => <Task key={task.id} task={task} />)}
-        </TaskList>
+        <Droppable droppableId={this.props.column.id}>
+          {provided => (
+            <TaskList
+              ref={provided.innerRef}
+              {...provided.droppableProps}
+            >
+              {this.props.tasks.map((task, index) => (
+                <Task key={task.id} task={task} index={index} />
+              ))}
+              {provided.placeholder}
+            </TaskList>
+          )}
+        </Droppable>
       </Container>
     )
   }
